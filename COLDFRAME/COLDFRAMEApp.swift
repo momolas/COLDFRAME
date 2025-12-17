444e47527254d6168fc2048d87b6f8b9e15dda45
Momo L'As <momolas@users.noreply.github.com>
2025-12-17 19:29:52 +0100

    Initial Commit

---
+++ COLDFRAME/COLDFRAMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  COLDFRAMEApp.swift
+//  COLDFRAME
+//
+//  Created by Mo on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct COLDFRAMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
