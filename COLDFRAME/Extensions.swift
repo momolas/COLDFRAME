bce950c9e86c6cb5f8a81e523bbadefb508f69fe
Momo L'As <momolas@users.noreply.github.com>
2026-03-14 17:33:01 +0100

    chore: Clean up Xcode build artifacts, add build output log, and modify Extensions.swift.

--- COLDFRAME/Extensions.swift
+++ COLDFRAME/Extensions.swift
@@ -7,12 +7,6 @@
 
 import SwiftUI
 
-// Thème de couleurs
-extension Color {
-	static let gold = Color(red: 0.83, green: 0.68, blue: 0.21)
-	static let darkBackground = Color(red: 0.05, green: 0.05, blue: 0.1)
-}
-
 // Modèle de données pour une prière
 struct PrayerTime: Identifiable, Hashable {
 	let id = UUID()
